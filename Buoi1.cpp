4f91bbb4e6b75ecec935f22b7c6be6af8513fc19
huyennk6 <huyennk6@fpt.edu.vn>
2025-11-03 15:58:36 +0700

    buoi 1

---
+++ Buoi1.cpp
@@ -0,0 +1,23 @@
+//them thu vien
+#include <stdio.h>
+
+//hàm main chính de chay chuong trinh
+int main(){
+	//in ra màn hình dòng chu Hello world
+	printf("Hello world!! \n");
+	//ket thuc cau lenh = dau cham phay ;
+	//khai bao bien tuoi- kieu so nguyen: short, int, long
+	int tuoi;
+	//khai bao bien namSinh- khoi tao gia tri- so nguyen
+	int namSinh = 2005;
+	//in bien ra man hinh
+	printf("nam sinh la: %d \n", namSinh);//%d- int
+	//khai bao bien diem- khoi tao gia tri- so thuc: float, double
+	double diem = 8.2;
+	printf("diem la: %lf \n", diem);//
+	return 0;//tra ve 0
+}
+
+
+
+
